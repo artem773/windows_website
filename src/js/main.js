a096856edc4de04dcf9eb10e0a2f2f0f3b504f77
artem <example@gmail.com>
2023-02-18 17:54:10 +0200

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,19 @@ import './slider';
 import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', ()=>{
 
     'use strict';
 
+    let modalState = {};
+    let deadline = '2023-03-29';
+
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
-    forms();
+    forms(modalState);
+    timer('.container1' ,deadline);
 
 });
